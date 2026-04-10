0f298cb2e7f080b134d6f37a7e213a544fb51bb2
ManyPro <giovannymanriquelol@gmail.com>
2026-04-10 10:01:39 -0500

    sas

--- Frontend/assets/js/theme-engine.js
+++ Frontend/assets/js/theme-engine.js
@@ -97,7 +97,7 @@
     }
     const link = document.createElement("link");
     link.rel = "stylesheet";
-    link.href = "./assets/ui.css?v=20260340";
+    link.href = "./assets/ui.css?v=20260406";
     head.appendChild(link);
   }
 
